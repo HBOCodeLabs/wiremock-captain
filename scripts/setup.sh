4826f35d5715ef4efe11f6e38e0bb2da2365be6f
Satvik Shukla <satvik.shukla@warnermedia.com>
2022-03-22 16:19:02 -0700

    nit 2

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-curl -H "Accept: application/zip" https://repo1.maven.org/maven2/org/wiremock/wiremock-webhooks-extension/2.31.0/wiremock-webhooks-extension-2.31.0.jar -o $PWD/extensions/wiremock-webhooks-extension-2.31.0.jar
+mkdir "$PWD"/extensions
+
+curl -H "Accept: application/zip" https://repo1.maven.org/maven2/org/wiremock/wiremock-webhooks-extension/2.31.0/wiremock-webhooks-extension-2.31.0.jar -o "$PWD"/extensions/wiremock-webhooks-extension-2.31.0.jar
 
 docker network create wiremock-net
 
-docker run -itd --rm --name wiremock-container-one -p 8080:8080 --network wiremock-net -v $PWD/extensions:/var/wiremock/extensions wiremock/wiremock:2.31.0 --record-mappings --verbose --extensions org.wiremock.webhooks.Webhooks
+docker run -itd --rm --name wiremock-container-one -p 8080:8080 --network wiremock-net -v "$PWD"/extensions:/var/wiremock/extensions wiremock/wiremock:2.31.0 --record-mappings --verbose --extensions org.wiremock.webhooks.Webhooks
 
 docker run -itd --rm --name wiremock-container-two -p 8081:8080 --network wiremock-net wiremock/wiremock:2.31.0 --record-mappings --verbose
 
