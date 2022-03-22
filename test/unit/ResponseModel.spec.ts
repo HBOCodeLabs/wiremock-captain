88d9f384393c52948399b2cce2e451a121518c34
Satvik Shukla <satvik.shukla@warnermedia.com>
2022-03-22 10:16:28 -0700

    support delays

--- test/unit/ResponseModel.spec.ts
+++ test/unit/ResponseModel.spec.ts
@@ -3,7 +3,7 @@
 
 import { beforeEach, describe, expect, it, jest } from '@jest/globals';
 
-import { BodyType } from '../../src';
+import { BodyType, DelayType } from '../../src';
 
 describe('ResponseModel', () => {
     beforeEach(() => {
@@ -17,7 +17,10 @@ describe('ResponseModel', () => {
             const mockedResponse = testModule.createWireMockResponse({
                 status: 200,
             });
-            expect(mockedResponse).toEqual({ status: 200 });
+            expect(mockedResponse).toEqual({
+                status: 200,
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+            });
         });
 
         it('builds with statusCode and body', () => {
@@ -28,6 +31,7 @@ describe('ResponseModel', () => {
             });
             expect(mockedResponse).toEqual({
                 status: 200,
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
                 jsonBody: { testKey: 'test-value' },
             });
         });
@@ -43,6 +47,7 @@ describe('ResponseModel', () => {
             );
             expect(mockedResponse).toEqual({
                 status: 200,
+                headers: {},
                 body: { testKey: 'test-value' },
             });
         });
@@ -56,6 +61,95 @@ describe('ResponseModel', () => {
             expect(mockedResponse).toEqual({
                 status: 200,
                 headers: { Accept: 'json', 'Content-Type': 'application/json; charset=utf-8' },
+            });
+        });
+
+        it('should build response with chunked dribble delay', () => {
+            const testModule: typeof import('../../src/ResponseModel') = require('../../src/ResponseModel');
+            const mockedResponse = testModule.createWireMockResponse(
+                {
+                    status: 200,
+                },
+                {
+                    responseDelay: {
+                        type: DelayType.CHUNKED_DRIBBLE,
+                        numberOfChunks: 1,
+                        totalDuration: 100,
+                    },
+                },
+            );
+            expect(mockedResponse).toEqual({
+                status: 200,
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+                chunkedDribbleDelay: {
+                    numberOfChunks: 1,
+                    totalDuration: 100,
+                },
+            });
+        });
+
+        it('should build response with fixed delay', () => {
+            const testModule: typeof import('../../src/ResponseModel') = require('../../src/ResponseModel');
+            const mockedResponse = testModule.createWireMockResponse(
+                {
+                    status: 200,
+                },
+                { responseDelay: { type: DelayType.FIXED, constantDelay: 100 } },
+            );
+            expect(mockedResponse).toEqual({
+                status: 200,
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+                fixedDelayMilliseconds: 100,
+            });
+        });
+
+        it('should build response with log normal delay', () => {
+            const testModule: typeof import('../../src/ResponseModel') = require('../../src/ResponseModel');
+            const mockedResponse = testModule.createWireMockResponse(
+                {
+                    status: 200,
+                },
+                {
+                    responseDelay: {
+                        type: DelayType.LOG_NORMAL,
+                        median: 5,
+                        sigma: 10,
+                    },
+                },
+            );
+            expect(mockedResponse).toEqual({
+                status: 200,
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+                delayDistribution: {
+                    type: 'lognormal',
+                    median: 5,
+                    sigma: 10,
+                },
+            });
+        });
+
+        it('should build response with log normal delay', () => {
+            const testModule: typeof import('../../src/ResponseModel') = require('../../src/ResponseModel');
+            const mockedResponse = testModule.createWireMockResponse(
+                {
+                    status: 200,
+                },
+                {
+                    responseDelay: {
+                        type: DelayType.UNIFORM,
+                        lower: 5,
+                        upper: 10,
+                    },
+                },
+            );
+            expect(mockedResponse).toEqual({
+                status: 200,
+                headers: { 'Content-Type': 'application/json; charset=utf-8' },
+                delayDistribution: {
+                    type: 'uniform',
+                    lower: 5,
+                    upper: 10,
+                },
             });
         });
     });
